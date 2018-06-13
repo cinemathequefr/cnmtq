2d58891aaad7cfe95036b79ee41b280df71c1ccf
nltesown <nlte@magic.fr>
2018-06-13 17:39:23 +0200

    Add YoY total + css new type

--- apps/stats/controllers/progression.js
+++ apps/stats/controllers/progression.js
@@ -17,85 +17,68 @@ module.exports = async function(ctx, next) {
   ctx.type = "text/html; charset=utf-8";
 
   try {
-    var monthTo = moment()
-      .startOf("month")
-      .subtract(1, "months")
-      .endOf("month"); // Mois précédent le mois courant
-
-    var monthFrom = monthTo.clone().startOf("year");
-
-    // Année n
-    var data0 = _(seances(db.getState(), monthFrom, monthTo, "month"))
+    var yoy0 = yoy(0);
+    var data0 = _(seances(db.getState(), yoy0.start, yoy0.end, "month"))
       .map((v, k) => [k, v.global.entrees])
       .sortBy(d => d[0])
       .value();
 
-    // Année n-1
-    var data1 = _(
-      seances(
-        db.getState(),
-        monthFrom.clone().subtract(1, "year"),
-        monthTo.clone().subtract(1, "year"),
-        "month"
-      )
-    )
+    var yoy1 = yoy(1);
+    var data1 = _(seances(db.getState(), yoy1.start, yoy1.end, "month"))
       .map((v, k) => [k, v.global.entrees])
       .sortBy(d => d[0])
       .value();
 
     var data = _([data0, data1])
       .unzip()
-      .map(d => _.flatten(d))
+      .map(_.flatten)
+      // .map(d => _.flatten(d))
       .map(d => {
         d.push((d[1] - d[3]) / d[3]);
         return d;
       })
       .value();
 
-    data = _({})
-      .assign(extendDataForViews(data), { monthTo: monthTo })
+    var totalYoy = [_.sumBy(data, "1"), _.sumBy(data, "3")];
+
+    // Après avoir calculé le total en année glissante, on élimine les données antérieures à l'année de référence, qu'on n'utilisera plus.
+    // On calcule pour cela l'année de référence.
+    var maxYear = moment(_(data).maxBy(d => moment(d[0]).year())[0]).year();
+    data = _(data)
+      .filter(d => moment(d[0]).year() === maxYear)
       .value();
+
+    data = _({}).assign(extendDataForViews(data), { yoy: totalYoy }).value();
+    console.log(data);
+    // data = extendDataForViews(data);
 
     return ctx.render("progression", data);
   } catch (e) {
     console.log(e);
   }
-
-  /*
-  try {
-    queryDate =
-      ctx.params.date ||
-      db
-        .map(d => d.date)
-        .max()
-        .value()
-        .substring(0, 10); // TODO: validation du paramètre
-
-    data = db
-      .filter(d => {
-        return d.date.substring(0, 10) === queryDate;
-      })
-      .map(d =>
-        _({})
-          .assign(d, {
-            salle: _(d.salle)
-              .assign({ capacity: config.capacity[d.salle.id] })
-              .value(),
-            tickets: _(d.tickets)
-              .assign({ tarifCat: tarifCat(d.tickets.tarif, config.tarifCats) })
-              .value()
-          })
-          .value()
-      )
-      .value();
-
-    data = _({})
-      .assign(extendDataForViews(data), { date: queryDate })
-      .value();
-
-    return ctx.render("day", data);
-  } catch (e) {
-    console.log(e);
-  }
-*/
 };
+
+/**
+ * yoy (year-on-year)
+ * Obtention des dates de début et fin d'année glissante jusqu'à la fin du mois échu (par rapport à la date courante)
+ * TODO: pouvoir passer en paramètre la date servant de référence (qu'elle ne soit pas forcément la date courante)
+ * @param negOffsetY {integer} Obtention de la même période pour l'année n moins offset.
+ * @return {Object: Moment, Moment}
+ */
+function yoy(negOffsetY) {
+  negOffsetY = Math.abs(negOffsetY || 0);
+  var ref = moment()
+    .startOf("month")
+    .subtract(1, "months")
+    .endOf("month");
+  var end = ref.clone().subtract(negOffsetY, "years");
+  var start = ref
+    .clone()
+    .subtract(negOffsetY + 1, "years")
+    .add(1, "day")
+    .startOf("day");
+  return {
+    start: start,
+    end: end
+  };
+}
