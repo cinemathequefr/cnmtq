44622f7c5e202287b0282d19db2f09d4292a4bf7
nltesown <nlte@magic.fr>
2018-09-11 12:55:31 +0200

    Synchro des seances a venir et throttling

--- apps/stats/controllers/preventes.js
+++ apps/stats/controllers/preventes.js
@@ -2,8 +2,8 @@ const _ = require("lodash");
 const moment = require("moment");
 const dbFuture = require("../services/db")("future");
 const config = require("../config");
-// const tarifCat = require("../lib/tarifCat");
 const extendDataForViews = require("../lib/extendDataForViews");
+const sync = require("../services/sync");
 
 module.exports = async function(ctx, next) {
   var data;
@@ -17,13 +17,61 @@ module.exports = async function(ctx, next) {
 
   try {
 
+    var nxt = nextSyncAllowed();
+
+    if (nxt.status === true) {
+      try {
+        await sync.future();
+      } catch (e) {
+        console.log("La synchronisation a échoué.");
+      }
+    }
+
     data = dbFuture.getState();
 
-    return ctx.render(
-      "preventes",
-      extendDataForViews(data)
-    );
+    return ctx.render("preventes", extendDataForViews(data, { nextSync: nxt }));
   } catch (e) {
     console.log(e);
   }
 };
+
+/**
+ * isValidFutureSyncRequest
+ * @return {boolean} La requête de synchro est valide (le délai minimum depuis la synchro précédente est passé)
+ */
+/*
+function isValidFutureSyncRequest() {
+  var 
+  return moment(
+    _(dbFuture.getState())
+      .keys()
+      .value()[0]
+  )
+    .add(config.syncThrottleMinutes, "minutes")
+    .isAfter(moment(), "second");
+}
+*/
+
+
+/*
+ * nextSyncAllowed
+ * A partir la date/heure de la dernière synchro (inscrite en clé de l'objet dbFuture) et du délai de throttling
+ * renvoie la date/heure de la prochaine synchro possible et indique si elle est déjà passée.
+ * @return {object}.status {boolean} : la synchro est possible
+ * @return {object}.dateTime {string|null} : si status:false, date/heure de la prochaine synchro
+ */
+function nextSyncAllowed() {
+  var nextSyncDateTime = moment(
+    _(dbFuture.getState())
+      .keys()
+      .value()[0]
+  ).add(config.sync.syncThrottleMinutes, "minutes");
+
+  var status = moment().isAfter(nextSyncDateTime, "second");
+  var dateTime = (status === false ? nextSyncDateTime.format() : null);
+
+  return {
+    status: status,
+    dateTime: dateTime
+  }
+}
