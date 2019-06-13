c26c93c2d2b78e35ef71481e4af184f6247c26e1
nltesown <nlte@magic.fr>
2019-06-13 14:27:30 +0200

    Fetch on demand fresh seances data for given date, view only, no write

--- apps/stats/misc/sync.js
+++ apps/stats/misc/sync.js
@@ -1,8 +1,20 @@
 const sync = require("../services/sync");
 
 (async () => {
-  var d = await sync.past();
-  console.log("Finished");
+
+  let date = process.argv[2]; // Date de la synchro a effectuer
+
+  if (!date) {
+    await sync.past();
+    console.log("Finished");
+  } else {
+
+    console.log(JSON.stringify(await sync.query(date, date), null, 2));
+
+
+
+  }
+
 })();
 
 // (async () => {
